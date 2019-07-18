e9f872dcc5e62422e8d43f837ac8c4be341fddd4
Paul Solt <PaulSolt@gmail.com>
2019-07-18 12:52:18 -0400

    For loop

--- iOS6-Pointers/iOS6-Pointers/main.m
+++ iOS6-Pointers/iOS6-Pointers/main.m
@@ -16,7 +16,7 @@ int main(int argc, const char * argv[]) {
         char letter = 't';
         printf("Character: %c\n", letter);
         
-        char *ptr = "Hello World!\n";
+        char *ptr = "Hello World!\n"; // String literal (readonly)
         printf("%s\n", ptr);        // printf = print formatted (using tokens)
         // %s = c-string = char *
         // %d = int
@@ -24,6 +24,18 @@ int main(int argc, const char * argv[]) {
         
         // 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\n', '\0'
         //  0    1    2    3    4    5    6    7    8    9    10   11   12    13
+        
+        letter = *(ptr + 6);
+        printf("%c\n", letter);
+        
+        // i++
+        // i = i + 1
+        printf("For Loop\n");
+        for (int i = 0; i < strlen(ptr); i++) {
+            letter = *(ptr + i);
+            printf("%c\n", letter);
+        }
+        
         
     }
     
