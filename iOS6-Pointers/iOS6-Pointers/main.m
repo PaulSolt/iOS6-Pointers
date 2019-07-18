7613e39a33c5f520f834cb0d74eaf421d3bac424
Paul Solt <PaulSolt@gmail.com>
2019-07-18 12:21:53 -0400

    Initial commit

---
+++ iOS6-Pointers/iOS6-Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iOS6-Pointers
+//
+//  Created by Paul Solt on 7/18/19.
+//  Copyright © 2019 Lambda, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
