e0fbaa87ce0902a187e308d3f53ea05cf6eeb650
Paul Solt <PaulSolt@gmail.com>
2019-07-18 12:34:13 -0400

    Added pointer logic

--- iOS6-Pointers/iOS6-Pointers/main.m
+++ iOS6-Pointers/iOS6-Pointers/main.m
@@ -10,8 +10,22 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        printf("Hello, World!\n");
+        
+        char letter = 't';
+        printf("Character: %c\n", letter);
+        
+        char *ptr = "Hello World!\n";
+        printf("%s\n", ptr);        // printf = print formatted (using tokens)
+        // %s = c-string = char *
+        // %d = int
+        // %@ = Objc object (only works with NSLog)
+        
+        // 'H', 'e', 'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\n', '\0'
+        //  0    1    2    3    4    5    6    7    8    9    10   11   12    13
+        
     }
-    return 0;
+    
+    return 0;   // exits with success, non-zero is not successful
 }
